d10b36c7f6ce54c3d26b8e98fed0247453c35d44
tuanpv29 <tuanpv29@gmail.com>
2021-06-01 23:50:55 +0700

    create footer, improve style, and add back top btn

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
-const backToTop = document.getElementById('backToTop');
-
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
 }
